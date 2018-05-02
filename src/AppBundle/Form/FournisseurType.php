d4f666aedbde68d8c61fdf8cca8fe2feec2704f1
Garali Mohamed Karim <garalimedkarim2@gmail.com>
2018-05-02 06:27:34 +0100

    init form Fournisseur

---
+++ src/AppBundle/Form/FournisseurType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+
+
+class FournisseurType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('name',TextType::class, array(
+        ))
+        ->add('pays',CountryType::class, array(
+          'data' => 'TN'
+        ))
+        ->add('tel',TelType::class, array(
+        ));
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Fournisseur'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_fournisseur';
+    }
+
+
+}
